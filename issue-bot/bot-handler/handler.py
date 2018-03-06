87b2a23b562b6574196b4f924f7b6132ccd665d7
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-03-06 08:52:40 +0000

    Update / refactor bot
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- issue-bot/bot-handler/handler.py
+++ issue-bot/bot-handler/handler.py
@@ -2,10 +2,10 @@ import requests, json, os, sys
 from github import Github
 
 def handle(req):
-
     event_header = os.getenv("Http_X_Github_Event")
 
     if not event_header == "issues":
+        print("Unable to handle X-GitHub-Event: " + event_header)
         sys.exit(1)
         return
 
@@ -14,17 +14,26 @@ def handle(req):
     payload = json.loads(req)
 
     if not payload["action"] == "opened":
+        print("Action not supported: " + payload["action"])
+        sys.exit(1)
         return
 
-    #sentimentanalysis
-    res = requests.post('http://' + gateway_hostname + ':8080/function/sentimentanalysis', data=payload["issue"]["title"]+" "+payload["issue"]["body"])
+    # Call sentimentanalysis
+    res = requests.post('http://' + gateway_hostname + ':8080/function/sentimentanalysis', 
+                        data= payload["issue"]["title"]+" "+payload["issue"]["body"])
 
-    # positive_threshold
+    # Read the positive_threshold from configuration
     positive_threshold = float(os.getenv("positive_threshold", "0.2"))
 
+    # Call back to GitHub to apply a label
+    apply_label(res.json()['polarity'], payload["issue"]["number"], payload["repository"]["full_name"])
+
+    print(res.json())
+
+def apply_label(polarity, issue_number, repo):
     g = Github(os.getenv("auth_token"))
-    repo = g.get_repo(os.getenv("repo"))
-    issue = repo.get_issue(payload["issue"]["number"])
+    repo = g.get_repo(repo)
+    issue = repo.get_issue(issue_number)
 
     has_label_positive = False
     has_label_review = False
@@ -34,9 +43,7 @@ def handle(req):
         if label == "review":
             has_label_review = True
 
-    if res.json()['polarity']  >  positive_threshold and not has_label_positive:
+    if polarity  >  positive_threshold and not has_label_positive:
         issue.set_labels("positive")
     elif not has_label_review:
         issue.set_labels("review")
-
-    print(res.json())
