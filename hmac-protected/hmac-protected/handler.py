9beb508f34501d961bfbdbcd7a9a64dfe93a7596
Martin Dekov (VMware) <mdekov@vmware.com>
2018-10-03 03:43:37 -0700

    Add lab11 with HMAC validation
    Adding lab11 which exmplains how to use HMAC to validate
    the payload of your functions
    
    Signed-off-by: Martin Dekov (VMware) <mdekov@vmware.com>

---
+++ hmac-protected/hmac-protected/handler.py
@@ -0,0 +1,30 @@
+import os, hmac, hashlib
+
+def validateHMAC(message, secret, hash):
+
+    # GitHub and the sign flag prefix the hash with "sha1="
+    receivedHash = clearHash(hash)
+
+    # Hash message with secret
+    expectedMAC = hmac.new(secret.encode(), message.encode(), hashlib.sha1)
+    createdHash = expectedMAC.hexdigest()
+
+    return receivedHash == createdHash
+
+def clearHash(hash):
+    if "sha1=" in hash:
+        hash=hash[5:]
+    return hash
+
+def handle(req):
+    # We receive the hashed message in form of a header
+    messageMAC = os.getenv("Http_Hmac")
+
+    # Read secret from inside the container
+    with open("/var/openfaas/secrets/payload-secret","r") as secretContent:
+        payloadSecret = secretContent.read()
+
+    # Function to validate the HMAC
+    if validateHMAC(req, payloadSecret, messageMAC):
+        return "Successfully validated: " + req
+    return "HMAC validation failed."
